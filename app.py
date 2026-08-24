aae8a99bbe98fa47386631c9109d42cd927d4a9c
VenkataSatyaSai16 <evenkatasatyasai@gmail.com>
2026-08-24 14:49:50 +0530

    Code Updated3

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return """<h1>Welcome to Flask API!</h1>
-              <a href='rgukt.ac.in'>RGUKT</a>"""
+              <a href='https://rgukt.ac.in'>RGUKT</a>"""
 
 @app.route("/api/hello")
 def hello():
     return {
-        "message": "<h1>Hello from Flask API!</h1> <a href='rgukt.ac.in'>RGUKT</a>",
+        "message": "<h1>Hello from Flask API!</h1> <a href='https://rgukt.ac.in'>RGUKT</a>",
         "status": "success"
     }
 
